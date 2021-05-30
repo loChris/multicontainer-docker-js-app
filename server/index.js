ee4d296acac1f72722095211ee88c41b42ba6790
loChris <chistian.lopezocasio@gmail.com>
2021-05-30 13:37:51 -0400

    added setup code for server configuration

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-import keys from './keys';
+import keys from './keys.js';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { Pool } from 'pg';
+import redis from 'redis';
+import pkg from 'pg';
 
 // Express App Setup
 const app = express();
@@ -10,7 +11,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Postgres Client Setup
-pgClient = new Pool({
+const { Pool } = pkg;
+
+const pgClient = new Pool({
 	user: keys.pgUser,
 	host: keys.pgHost,
 	database: keys.pgDatabase,
@@ -24,4 +27,46 @@ pgClient.on('connect', (client) => {
 	client
 		.query('CREATE TABLE IF NOT EXISTS values (number INT)')
 		.catch((err) => console.error(err));
+});
+
+// Redis Client Setup
+const redisClient = redis.createClient({
+	host: keys.redisHost,
+	port: keys.redisPort,
+	retry_strategy: () => 1000,
+});
+const redisPublisher = redisClient.duplicate();
+
+// Express Route Handlers
+app.get('/', (req, res) => {
+	res.send('Hello world!');
+});
+
+app.get('/values/all', async (req, res) => {
+	const values = await pgClient.query('SELECT * from values');
+	res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+	redisClient.hgetall('values', (err, values) => {
+		res.send(values);
+	});
+});
+
+app.post('/values', async (req, res) => {
+	const { index } = req.body;
+
+	if (parseInt(index) > 40) {
+		return res.status(422).send('Index too high');
+	}
+
+	redisClient.hset('values', index, 'Nothing yet...');
+	redisPublisher.publish('insert', index);
+	pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+	res.send({ working: true });
+});
+
+app.listen(5000, (err) => {
+	console.log('Listening...');
 });
