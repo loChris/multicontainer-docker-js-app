111ecc09757ab9167fa66ce3337ab6b04f56fad3
loChris <chistian.lopezocasio@gmail.com>
2021-05-30 14:00:14 -0400

    wip: started adding logic to fib file and created a generic page just to include some routing

---
+++ client/src/other-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const otherPage = () => {
+	return (
+		<div>
+			I'm some other page!
+			<Link to="/">Go back home</Link>
+		</div>
+	);
+};
